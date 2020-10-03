69918f20669753116b1014cb63da48567cca32da
mbaros <mbaros@email.com>
2020-10-03 22:36:26 +0200

    uvodni komit nalezeni princezny

---
+++ src/test/java/ai/SavePrincessTest.java
@@ -0,0 +1,22 @@
+package ai;
+
+import java.util.Scanner;
+
+public class SavePrincessTest {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        try (Scanner scan = new Scanner(System.in)) {
+
+            int m = scan.nextInt();
+
+            scan.nextLine();
+
+            for (int i = 0; i < count; i++) {
+
+                texts.add(scan.nextLine());
+
+
+            }
+}
