f2e01edb06b87ee0e565400943602826bbf2224d
mbaros <mbaros@email.com>
2020-10-05 23:49:55 +0200

    nacitani vstupniho gridu z console

--- src/test/java/ai/SavePrincessTest.java
+++ src/test/java/ai/SavePrincessTest.java
@@ -1,22 +1,169 @@
 package ai;
 
+import java.awt.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class SavePrincessTest {
+class SavePrincessTest {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 
+
+        List<List<String>> grid = null;
+        try {
+//            grid = generateGridFromScanner();
+            grid = generateGridFromIO();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+
+        if (grid != null && grid.size() >= 3 && grid.size() < 100) {
+            grid.forEach(r -> {
+                r.forEach(System.out::print);
+                System.out.println();
+            });
+        } else {
+            System.err.println("Grid has not size 3 <= N < 100");
+        }
+    }
+
+    private static List<List<String>> generateGridFromScanner() throws Exception {
+        int dim = 0;
+
+        List<List<String>> grid = null;
+        List<String> row;
+
         try (Scanner scan = new Scanner(System.in)) {
 
-            int m = scan.nextInt();
+            dim = scan.nextInt();
+
+            grid = new ArrayList<>(dim);
 
             scan.nextLine();
 
-            for (int i = 0; i < count; i++) {
+            Point bot = null;
+            Point princess = null;
 
-                texts.add(scan.nextLine());
+            char nextChar;
+            char[] rowChars;
 
+            for (int i = 0; i < dim; i++) {
+                row = new ArrayList<>(dim);
+                rowChars = scan.next().toCharArray();
+                for (int j = 0; j < dim; j++) {
+
+                    nextChar = rowChars[j];
+                    switch (nextChar) {
+                        case 'm':
+                            if (bot != null) {
+                                throw new Exception("Bot is already set!");
+                            }
+
+                            bot = new Point(i, j);
+                            break;
+                        case 'p':
+                            if (princess != null) {
+                                throw new Exception("Princess is already set!");
+                            }
+
+                            princess = new Point(i, j);
+                            break;
+                        case '-':
+                            row.add(String.valueOf(nextChar));
+                            break;
+                        default:
+                            j--;//stay at the same index
+
+                            break;
+                    }
+
+                }
+
+                scan.nextLine();
+
+                if (row.size() != dim) {
+                    throw new Exception(String.format("Row %d has not size %d of set grid row %d", i, row.size(), dim));
+                }
+
+                grid.add(row);
 
             }
+        }
+
+        return grid;
+    }
+
+    private static List<List<String>> generateGridFromIO() throws Exception {
+        int dim = 0;
+
+        List<List<String>> grid = null;
+        List<String> row;
+
+        byte[] dimBytes = System.in.readNBytes(1);
+        dim = Integer.valueOf(new String(dimBytes, StandardCharsets.UTF_8));
+
+        grid = new ArrayList<>(dim);
+
+        //read a new line
+//        System.in.read();
+        System.in.skip(1);
+
+        Point bot = null;
+        Point princess = null;
+
+        char nextChar;
+        char[] rowChars;
+
+        for (int i = 0; i < dim; i++) {
+            row = new ArrayList<>(dim);
+            //dim + 1 = escape new line char
+            rowChars = new String(System.in.readNBytes(dim ), StandardCharsets.UTF_8).toCharArray();
+            //read a new line
+//            System.in.read();
+
+            if(rowChars.length < dim){
+                throw new Exception("You have set less columns then " + dim);
+            }
+            for (int j = 0; j < dim; j++) {
+
+                nextChar = rowChars[j];
+                switch (nextChar) {
+                    case 'm':
+                        if (bot != null) {
+                            throw new Exception("Bot is already set!");
+                        }
+
+                        bot = new Point(i, j);
+                        break;
+                    case 'p':
+                        if (princess != null) {
+                            throw new Exception("Princess is already set!");
+                        }
+
+                        princess = new Point(i, j);
+                        break;
+                    case '-':
+                        row.add(String.valueOf(nextChar));
+                        break;
+                    default:
+                        throw new Exception("Wrong char! You can set only -, 1xm, 1xp!");
+                }
+
+            }
+
+
+            if (row.size() != dim) {
+                throw new Exception(String.format("Row %d has not size %d of set grid row %d", i, row.size(), dim));
+            }
+
+            grid.add(row);
+
+        }
+
+        return grid;
+    }
 }
--- src/test/java/thirtycodingdays/ScannerIOTest.java
+++ src/test/java/thirtycodingdays/ScannerIOTest.java
@@ -1,81 +1,81 @@
 package thirtycodingdays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ScannerIOTest {
 
-	//	https://www.hackerrank.com/challenges/30-hello-world/tutorial
+    //	https://www.hackerrank.com/challenges/30-hello-world/tutorial
 
-	static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int count;
+        int count;
 
-		List<String> texts = new ArrayList<>();
+        List<String> texts = new ArrayList<>();
 
-		try (Scanner scan = new Scanner(System.in)) {
+        try (Scanner scan = new Scanner(System.in)) {
 
-			count = scan.nextInt();
+            count = scan.nextInt();
 
-			scan.nextLine();
+            scan.nextLine();
 
-			for (int i = 0; i < count; i++) {
+            for (int i = 0; i < count; i++) {
 
-				texts.add(scan.nextLine());
+                texts.add(scan.nextLine());
 
-			}
-		}
+            }
+        }
 
-		List<String> results = getEvenAndOddChars(count, texts);
+        List<String> results = getEvenAndOddChars(count, texts);
 
-		results.forEach(System.out::println);
-	}
+        results.forEach(System.out::println);
+    }
 
-	private static List<String> getEvenAndOddChars(int count, List<String> inputStrings) {
+    private static List<String> getEvenAndOddChars(int count, List<String> inputStrings) {
 
-		List<String> resultArray = new ArrayList<>(count);
+        List<String> resultArray = new ArrayList<>(count);
 
-		char[] chars;
+        char[] chars;
 
-		StringBuilder evenSb;
-		StringBuilder oddSb;
+        StringBuilder evenSb;
+        StringBuilder oddSb;
 
-		for (String inputText : inputStrings) {
-			chars = inputText.toCharArray();
+        for (String inputText : inputStrings) {
+            chars = inputText.toCharArray();
 
-			evenSb = new StringBuilder();
-			oddSb = new StringBuilder();
+            evenSb = new StringBuilder();
+            oddSb = new StringBuilder();
 
-			for (int i = 0; i < chars.length; i++) {
-				if (i % 2 == 0) {
-					//even char
-					evenSb.append(chars[i]);
-				} else {
-					//even char
-					oddSb.append(chars[i]);
-				}
-			}
+            for (int i = 0; i < chars.length; i++) {
+                if (i % 2 == 0) {
+                    //even char
+                    evenSb.append(chars[i]);
+                } else {
+                    //even char
+                    oddSb.append(chars[i]);
+                }
+            }
 
-			resultArray.add(evenSb.toString() + " " + oddSb.toString());
-		}
+            resultArray.add(evenSb.toString() + " " + oddSb.toString());
+        }
 
-		return resultArray;
-	}
+        return resultArray;
+    }
 
-	@Test
-	void readFromOutputTest() {
+    @Test
+    void readFromOutputTest() {
 
-		int count = 2;
+        int count = 2;
 
-		List<String> results = getEvenAndOddChars(count, List.of("Hacker", "Rank"));
+        List<String> results = getEvenAndOddChars(count, List.of("Hacker", "Rank"));
 
-		assertEquals(count, results.size());
-		assertEquals("Hce akr", results.get(0));
-		assertEquals("Rn ak", results.get(1));
-	}
+        assertEquals(count, results.size());
+        assertEquals("Hce akr", results.get(0));
+        assertEquals("Rn ak", results.get(1));
+    }
 }
