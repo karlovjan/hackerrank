f9a5a808554c41def0d6882d50f64184d1359b60
baros <baros@fio.cz>
2021-03-08 14:40:43 +0100

    stream grouping by Bird type max

--- src/test/java/util/UtilTest.java
+++ src/test/java/util/UtilTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
