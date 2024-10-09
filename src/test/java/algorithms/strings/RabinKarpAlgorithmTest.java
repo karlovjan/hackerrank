6beb7f02359c9d880d8d8db2280736fc752d0811
mbaros <karlovjan@gmail.com>
2024-10-09 16:47:31 +0200

    RabinKarpAlgorithmTest create hash function

--- src/test/java/algorithms/strings/RabinKarpAlgorithmTest.java
+++ src/test/java/algorithms/strings/RabinKarpAlgorithmTest.java
@@ -1,6 +1,9 @@
 package algorithms.strings;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,4 +14,22 @@ import org.junit.jupiter.api.Test;
  * Dynamic Programming Algorithms
  */
 public class RabinKarpAlgorithmTest {
+
+    List<Integer> rubinKarp(String text, String pattern) {
+      var foundList = new ArrayList<>();
+      
+      
+    }
+
+
+    int getHash(String text, int base){
+        int textLength = text.length();
+        int hash = 0;
+        for (int i = 0; i < textLength; i++) {
+            hash += ((int)text.charAt(i))*Math.pow(base, textLength - (i + 1));
+        }
+
+        return hash;
+    }
+
 }
